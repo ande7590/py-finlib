8a5cb1ae7ccc1954c57d3351230f379352870fec
Mike McPhee Anderson <mr.anderson.1725@gmail.com>
2019-03-23 00:20:00 -0500

    tweaked code, unittests next

--- finlib/loan.py
+++ finlib/loan.py
@@ -1,7 +1,12 @@
 from typing import NamedTuple, Union, Generator
 
+class LoanBalance(NamedTuple):
+    current_period: int
+    current_balance: float
+    last_payment: float
+
 PMT_TYPE = Union[float, list]
-AMORT_TYPE = Generator[LoanBalance]
+AMORT_TYPE = Generator[LoanBalance, None, None]
 
 class LoanAmortization(object):    
     """Computes the balance and repayment schedule of loans and similar debts."""
@@ -11,24 +16,23 @@ class LoanAmortization(object):
         self.term = term 
         self.interest = interest
         self.loan_amt = loan_amt
-        self.ending_balance = ending_balance
         self.first_pmt_now = first_pmt_now
         self.__mutable = False
 
     def __setattr__(self, name, value):
-        if self.__mutable:
-            super.__setattr__(name, value)
+        if not hasattr(self, '__mutable') or self.__mutable:
+            super.__setattr__(self, name, value)
         else:
             raise AttributeError("LoanAmortization is immutable, cannot set %s" % (name))            
 
     def amortization(self) -> AMORT_TYPE:
         """Performs a projection using the payment implied by the parameters 
         in the constructor, which results in zero ending balance (loan fully repaid)"""
-        self.project_from_balance(self, self.loan_amt)
+        return self.amortization_from_pmts(self.level_pmt)
 
     def amortization_from_pmts(self, pmts: PMT_TYPE) -> AMORT_TYPE:
         """Performs a projection using a schedule of arbitrary payments."""        
-        self.project_from_balance(self, self.loan_amt, current_period=0, pmts=pmts)
+        return self.amortization_from_balance(self.loan_amt, 0, pmts)
                 
     def amortization_from_balance(self, current_balance: float, current_period: int=0, pmts: PMT_TYPE=None) -> AMORT_TYPE:
         """Performs a projection starting from the supplied balance.  The payment
@@ -37,7 +41,7 @@ class LoanAmortization(object):
         remaining_loan_periods = self.term - current_period
         
         if pmts is not list:
-            pmts = [if pmts is float pmts else self.level_pmt] * remaining_loan_periods                
+            pmts = [pmts if pmts is float else self.level_pmt] * remaining_loan_periods
 
         while len(pmts) > 0:
             current_payment = pmts.pop(0)            
@@ -45,21 +49,12 @@ class LoanAmortization(object):
             current_balance -= current_payment
             yield LoanBalance(current_period = self.term - len(pmts),
              current_balance = current_balance,
-             last_payment = current_payment)
-        
-        raise StopIteration()
-    
+             last_payment = current_payment)        
+
     @property
     def level_pmt(self):
-        disc_factor = pow(self.interest, -1 * self.term)
+        disc_factor = pow(1 + self.interest, -1 * self.term)
         annuity_factor = (1 - disc_factor) / self.interest
-        if self.first_pmt_now == False:
-            annuity_factor *= (1 + self.interest)
-        return annuity_factor
-
-
-class LoanBalance(NamedTuple):
-    current_period: int
-    current_balance: float
-    last_payment: float
-
+        if self.first_pmt_now == True:
+            annuity_factor /= (1 + self.interest)
+        return self.loan_amt / annuity_factor
---
+++ test/test_loan.py
@@ -0,0 +1,3 @@
+import unittest
+from finlib.loan import LoanAmortization
+
